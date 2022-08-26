72676d6d12ecbb2830845974c027feee40f90b52
abish <abish@>
2022-08-26 22:54:21 +0500

    Добавьте файлы проекта.

---
+++ StringLab/Class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace StringLab
+{
+    class Class
+    {
+        static public void Print(string str)
+        {
+
+            Console.WriteLine(str);
+        }
+        static public void StrR(string str)
+        {
+            var stroka =str;
+
+            const int interval = 1; // вставка пробела каждые interval символов
+
+            var result = new StringBuilder();
+            for (int i = 0; i < stroka.Length - 1; ++i)
+            {
+                result.Append(stroka[i]);
+                if ((i + 1) % interval == 0)
+                    result.Append(' ');
+            }
+
+            if (stroka.Length > 0)
+                result.Append(stroka[stroka.Length - 1]);
+
+            Console.WriteLine(result.ToString());
+            
+        }
+        static public void Sym(string str)
+        {
+            Console.WriteLine("Колличество символовв строке - {0} = {1}", str, str.Length);
+        }
+
+    }
+}
---
+++ StringLab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StringLab
+{
+    delegate void myString(string myStr);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str1 = Console.ReadLine();
+            myString print = Class.Print;
+            print += Class.StrR;
+            print += Class.Sym;
+            print(str1);
+
+        }
+    }
+}
